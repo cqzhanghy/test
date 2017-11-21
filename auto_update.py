a65f34d62790d83fa242c4b2fecdb906009d48a7
cqzhanghy <907843@qq.com>
2017-11-21 17:54:42 +0800

    4

--- auto_update.py
+++ auto_update.py
@@ -1,8 +1,15 @@
 import git
 repo = git.Repo.init("e:/test/test2")
-repo.remotes.origin.fetch()
-print("master:",repo.commit('master'))
-print("origin/master:",repo.commit('origin/master'))
-if repo.commit('master') != repo.commit('origin/master'):
+w = "master"
+r = "origin/master"
+repo.remotes.origin.fetch("master")
+repo.git.checkout(w)
+print("master:",repo.commit(w))
+print("origin/master:",repo.commit(r))
+if repo.commit(w) != repo.commit(r):
     print("need update")
+    print("process update.....")
+else:
+    print("It's up to date")
+    repo.git.merge(r)
 
